9a53f98a7a5a9256aef02ddd0afd5126bfd59096
ffraniel <francis.whitehead@googlemail.com>
2019-06-28 01:07:33 +0100

    added very basic css animation and transitions

--- menu-script.js
+++ menu-script.js
@@ -4,3 +4,15 @@ let backButton = document.querySelector('.back-button');
 let lastPage = localStorage.getItem('lastPage') || 'file:///home/francis/Code/Sandbox/testmenu/index.html';
 
 backButton.href = lastPage;
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('body').classList.add('fade-out');
+      setTimeout(() => {
+        window.location = link.href;
+      }, 500);
+  })
+})
--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const thisPage = window.location.href;
 localStorage.setItem('lastPage', thisPage);
 
 let checkWorked = localStorage.getItem('lastPage');
+
+//optional link-jacking
+const menuBurger = document.querySelector('.menu-burger');
+
+menuBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('body').classList.add('fade-out');
+  setTimeout(() => {
+    window.location = menuBurger.href;
+  }, 500);
+});
